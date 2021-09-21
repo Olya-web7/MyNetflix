ca561a6536dea38fa7a5e8b53b8fd1be5d74e85b
Olya <shyshova24@gmail.com>
2021-09-21 11:42:32 +0300

    browse

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -20,9 +20,28 @@ export default function SignUp() {
     const handleSignup = (event) => {
         event.preventDefault();
 
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(emailAddress, password)
+          .then((result) =>
+            result.user
+              .updateProfile({
+                  displayName: firstName,
+                  photoURL: Math.floor(Math.random() * 5) + 1,
+              }).then(() => {
+                  history.push(ROUTES.BROWSE);
+            })
+          )
+          .catch((error) => {
+              setPassword('');
+              setEmailAddress('');
+              setPassword('');
+              setError(error.message);
+        });
     };
 
     return (
+      <>
         <HeaderContainer>
             <Form>
             <Form.Title>Sign Up</Form.Title>
@@ -49,9 +68,16 @@ export default function SignUp() {
                     <Form.Submit disabled={isInvalid} type='submit'>
                         Sign Up
                     </Form.Submit>
-
+                    <Form.Text>
+                        Already a user? <Form.Link to='/signin'>Sign in now.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+                    </Form.TextSmall>
                 </Form.Base>
             </Form>
         </HeaderContainer>
-    );
+        <FooterContainer />
+    </>
+  );
 }
