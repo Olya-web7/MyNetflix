ec2080822559617025decab4e7d77820798447ec
Olya <shyshova24@gmail.com>
2021-09-22 20:15:21 +0300

    final for 22sep

--- src/containers/browse2.js
+++ src/containers/browse2.js
@@ -4,14 +4,23 @@ import { FirebaseContext } from "../context/firebase";
 import {Card, Header, Loading} from '../components';
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
+import axios from "axios";
 
-export function BrowseContainer({ slides }) {
+export function BrowseContainer() {
 
-  const [category, setCategory] = useState('series');
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.tvmaze.com/shows')
+      .then(res => {
+        console.log(res.data)
+        setShows(res.data)
+      })
+  });
+
   const [searchTerm, setSearchTerm] = useState('');
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
-  const [slideRows, setSlideRows] = useState([]);
 
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
@@ -21,11 +30,7 @@ export function BrowseContainer({ slides }) {
     setTimeout(() => {
       setLoading(false);
     }, 3000);
-  }, [profile.displayName]);
-
-  useEffect(() => {
-    setSlideRows(slides[category]);
-  }, [slides, category]);
+  });
 
   return profile.displayName ? (
     <>
@@ -36,15 +41,6 @@ export function BrowseContainer({ slides }) {
 
           <Header.Group>
             <Header.Logo to={ROUTES.HOME} src={logo} alt='Netflix' />
-             <Header.TextLink
-              active={category === 'series' ? 'true' : 'false'}
-              onClick={() => setCategory('series')}
-            >
-              Series</Header.TextLink>
-             <Header.TextLink
-              active={category === 'films' ? 'true' : 'false'}
-              onClick={() => setCategory('films')}
-            >Films</Header.TextLink>
           </Header.Group>
 
           <Header.Group>
@@ -68,42 +64,20 @@ export function BrowseContainer({ slides }) {
         </Header.Frame>
         <Header.Feature>
           <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
-          <Header.Text>
-             Forever alone in a crowd, failed comedian Arthur Fleck seeks
-            connection as he walks the streets of Gotham City. Arthur wears two
-            masks -- the one he paints for his day job as a clown, and the guise
-            he projects in a futile attempt to feel like he's part of the world
-            around him.
-          </Header.Text>
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
 
       <Card.Group>
-        {slideRows.map((slideItem) => (
-          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
-            <Card.Title>{slideItem.title}</Card.Title>
-            <Card.Entities>
-              {slideItem.data.map((item) => (
-                <Card.Item key={item.docId} item={item}>
-                  <Card.Image
-                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
-                  />
-                  <Card.Meta>
-                    <Card.Subtitle>{item.title}</Card.Subtitle>
-                    <Card.Text>{item.description}</Card.Text>
-                  </Card.Meta>
-                </Card.Item>
-              ))}
-            </Card.Entities>
-            {/*<Card.Feature category={category}>*/}
-            {/*  <Player>*/}
-            {/*    <Player.Button />*/}
-            {/*    <Player.Video src='/videos/bunny.mp4' />*/}
-            {/*  </Player>*/}
-            {/*</Card.Feature>*/}
-          </Card>
-        ))}
+
+          {shows.map((item) => (
+            <div key={item.id}>
+              <img src={item.image.medium} alt='1'/>
+                <p>{item.name}</p>
+                <p>{item.rating.average}</p>
+            </div>
+          ))}
+
       </Card.Group>
     </>
     ) : (
