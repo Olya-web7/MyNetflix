a076574f6e9921a4fbe0611e8efe602f120df1bd
Olya <shyshova24@gmail.com>
2021-09-22 13:12:41 +0300

    before search

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,12 @@ import {
     Background,
     ButtonLink,
     Container,
+    Link,
     Logo,
+    Group,
+    Profile,
+    Dropdown,
+    Picture,
     Feature,
     FeatureCallOut,
     Text,
@@ -23,13 +28,35 @@ Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }
     return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
 };
 
+Header.Profile = function HeaderProfile({children, ...restProps}) {
+    return <Profile {...restProps}>{children}</Profile>
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+    return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
 Header.Text = function HeaderText({ children, ...restProps }) {
     return <Text {...restProps}>{children}</Text>;
 };
 
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>;
+};
+
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>
 };
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>
+};
+
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
     return <ButtonLink {...restProps}>{children}</ButtonLink>
