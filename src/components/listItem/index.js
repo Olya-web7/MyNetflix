8d7d9030d3ff8ff93e02c05389f2c18a81dc739f
Olya <shyshova24@gmail.com>
2021-09-29 16:05:56 +0300

    favourite remove

--- src/components/listItem/index.js
+++ src/components/listItem/index.js
@@ -1,20 +1,20 @@
-import './listItem.css';
-
-const ListItem = (props) => {
-  const FavouriteComponent = props.favouriteComponent;
-
-  return (
-    <div
-      className="image-container d-flex m-3">
-      <img src={props.image} alt='movie' />
-
-      <div
-        className='overlay d-flex align-items-center justify-content-center'>
-        <FavouriteComponent />
-      </div>
-
-    </div>
-  );
-};
-
-export default ListItem;
+// import './listItem.css';
+//
+// const ListItem = (props) => {
+//   const FavouriteComponent = props.favouriteComponent;
+//
+//   return (
+//     <div
+//       className="image-container d-flex m-3">
+//       <img src={props.image} alt='movie' />
+//
+//       <div
+//         className='overlay d-flex align-items-center justify-content-center'>
+//         <FavouriteComponent />
+//       </div>
+//
+//     </div>
+//   );
+// };
+//
+// export default ListItem;
--- src/containers/myPage.js
+++ src/containers/myPage.js
@@ -1,9 +1,10 @@
 // eslint-disable-next-line
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ShowsContext from '../context/shows/showsContext';
 import MovieListHeading from "../components/MovieListHeading";
 import AddFavourites from "../components/AddFavourites";
+import RemoveFavourites from "../components/RemoveFavourites";
 import './myPage.css';
 
 const MyPage = () => {
@@ -11,21 +12,43 @@ const MyPage = () => {
   const [favourites, setFavourites] = useState([]);
   const { shows } = showsContext;
 
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+      localStorage.getItem('favourites')
+    );
+
+    if (movieFavourites) {
+      setFavourites(movieFavourites);
+    }
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('favourites', JSON.stringify(items));
+  };
+
+
   const addFavouriteMovie = (show) => {
     const newFavouriteList = [...favourites, show];
     setFavourites(newFavouriteList);
-    // saveToLocalStorage(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
   };
+
+  const removeFavouriteMovie = (show) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.show.id !== show.show.id
+    );
+
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+
 
   return (
     <div className='movie-app container-fluid'>
 
-      {/*Movies----------------------------------------------------*/}
-
         <div className='m-4'>
           <MovieListHeading heading='Movies'/>
         </div>
-
         <div className='d-flex m-2 flex-wrap'>
           {shows.map((show) => (
                 <div className='image-container d-flex m-3' key={show.show.id}>
@@ -47,37 +70,33 @@ const MyPage = () => {
                     <h3 className='text-center'>{show.show.name}</h3>
                   </div>
                 </div>
-            )
-          )}
+            ))}
         </div>
 
-      {/*Favourites------------------------------------*/}
-
       <div className='m-4'>
-        <MovieListHeading heading='Favourites'/>
+        <MovieListHeading heading='My Favourites'/>
       </div>
-
       <div className='d-flex m-2 flex-wrap'>
-        {favourites.map((show) => (
-            <div className='image-container d-flex m-3' key={show.show.id}>
-
-              <img src={show.show.image
-                ? show.show.image.medium
+        {favourites.map((favourite) => (
+            <div className='image-container d-flex m-3' key={favourite.show.id}>
+              <img src={favourite.show.image
+                ? favourite.show.image.medium
                 : "https://t3.ftcdn.net/jpg/01/84/81/64/360_F_184816468_sXO2m7Xhy2xqENls5YxrKlmFg3Ii82Mr.jpg"
               }
-                   name={show.show.name}
-                   alt={show.show.name}
+                   name={favourite.show.name}
+                   alt={favourite.show.name}
               />
 
-              <div className='overlay d-flex align-items-center justify-content-center'>
-                <AddFavourites />
+              <div
+                onClick={() => removeFavouriteMovie(favourite)}
+                className='overlay d-flex align-items-center justify-content-center'>
+                <RemoveFavourites />
               </div>
               <div className='position-absolute top-0'>
-                <h3 className='text-center'>{show.show.name}</h3>
+                <h3 className='text-center'>{favourite.show.name}</h3>
               </div>
             </div>
-          )
-        )}
+          ))}
       </div>
 
     </div>
